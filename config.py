fea9062b6282664a812ef735e8e08e538f45ffd7
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-05-19 07:16:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,27 @@
-# --------------M----------------------------------
-
 import os
 from os import getenv
-# ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", ""))
-# ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "")
-# ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-# -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("")
-# ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-# ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-# ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100"))
-# ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srvter")
-# -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-100"))
+# -------------------- Telegram API Credentials --------------------
 
+API_ID = int(getenv("API_ID", "27775431"))
+API_HASH = getenv("API_HASH", "b70bb1d45a1d05236671d4cc615e40f9")
+
+# -------------------- Bot Token & Info ----------------------------
+
+BOT_TOKEN = getenv("BOT_TOKEN", "7930216260:AAHKqkMROgWrPeskWfYYSanLPV5x2DAkN5I")
+BOT_USERNAME = getenv("BOT_USERNAME", "@Pentin_extractorbot")
+
+# -------------------- Owner & Admins ------------------------------
+
+OWNER_ID = int(getenv("OWNER_ID", "2446676469"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "2446676469").split()))
+
+# -------------------- Database & Logging --------------------------
+
+MONGO_URL = getenv(
+    "MONGO_URL",
+    "mongodb+srv://Rohitmeena64:Ajmeer234590577@cluster0.mae8oyn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-2446676469"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-2446676469"))
